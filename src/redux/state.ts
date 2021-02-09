7475b00cb5ee8495c7c325d7da9628718e5dd7a7
Yehor Pliasov <fenix4088@gmail.com>
2021-02-09 09:22:36 +0100

    Added Redux-dev-tools for Counter

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,5 +1,6 @@
 import { combineReducers, createStore } from "redux";
 import { counterReducer } from "../Components/Counter/counterReducer";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({
   counterReducer,
@@ -9,9 +10,10 @@ export type rootStateT = ReturnType<typeof rootReducer>;
 
 let preloadState;
 const persistedTodosString = localStorage.getItem('Counter state')
-if (persistedTodosString) preloadState = JSON.parse(persistedTodosString)
+if (persistedTodosString) preloadState = JSON.parse(persistedTodosString);
 
-export const store = createStore(rootReducer, preloadState);
+
+export const store = createStore(rootReducer, preloadState, composeWithDevTools());
 
 store.subscribe(() => {
   localStorage.setItem("Counter state", JSON.stringify(store.getState()))
